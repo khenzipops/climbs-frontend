3a169fafbc83c3ab1dfa1ef8fce385bc27c74445
kpopera <kpopera123@gmail.com>
2025-07-05 20:19:41 +0800

    refactor: Update login page structure and remove legacy components
    - Refactored the login page to utilize the new LoginForm component, enhancing code clarity and maintainability.
    - Removed the old Login component to streamline the application structure.
    - Added a primary color variable in globals.css for consistent theming across the application.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,42 +1,9 @@
-import LoginForm from "@/components/Login";
-import Link from "next/link";
+import { LoginForm } from "@/components/login-form";
 
-export default function Home() {
+export default function LoginPage() {
   return (
-    <div className="">
-      <div className="min-h-screen bg-gray-100">
-        {/* Container for split screen */}
-        <div className="flex flex-col lg:flex-row w-full min-h-screen">
-          {/* Left side - Image/Content */}
-          <div className="lg:w-1/2 md:w-full sm:w-full bg-white flex items-center justify-center">
-            <img
-              src="/assets/background-image.jpg"
-              alt="Decorative"
-              className="w-full h-full object-cover" // Key styles
-            />
-          </div>
-
-          {/* Right side - Login Form */}
-          <div className="lg:w-1/2 md:w-full sm:w-full bg-white flex items-center justify-center">
-            <div className="w-full max-w-md">
-              <div className="flex justify-center mb-4">
-                <img
-                  src="/assets/climbs-logo.jpg"
-                  alt="Decorative"
-                  className="w-30 h-full object-cover" // Key styles
-                />
-              </div>
-              <div className="text-2xl font-bold mb-2 text-[#1e0e4b] text-center">
-                Welcome back to{" "}
-                <Link href="/climbs/dashboard">
-                  <span className="text-[#1c3df5]"> CLIMBS</span>
-                </Link>
-              </div>
-              <LoginForm />
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className="h-screen flex items-center justify-center">
+      <LoginForm />;
     </div>
   );
 }
