404e9da25d6c1fc5fe92b876b88739ee005512a5
kpopera <kpopera123@gmail.com>
2025-07-09 20:49:33 +0800

    feat: Revamp login page and form layout for improved user experience
    - Redesigned the login page to feature a two-column layout, enhancing visual appeal and usability.
    - Integrated a background image for a more engaging interface.
    - Updated the LoginForm component with a new logo and streamlined form structure, improving clarity and accessibility.
    - Added a "Login with Google" button for additional login options.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,22 @@
 import { LoginForm } from "@/components/login-form";
+import Image from "next/image";
 
 export default function LoginPage() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <LoginForm />;
+    <div className="grid h-screen w-full lg:grid-cols-2">
+      <div className="flex items-center justify-center">
+        <LoginForm />
+      </div>
+      <div className="hidden bg-gray-100 lg:block">
+        <Image
+          src="/assets/background-Image.jpg"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover"
+          priority
+        />
+      </div>
     </div>
   );
 }
